16e855e7f193a6b82d00fffa1791bfd215934587
Jean TALGORN--THOMAS <51022157+JeanTalgornThomas@users.noreply.github.com>
2021-06-23 15:35:56 +0200

    Affichage accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Articles;
+use App\Entity\Animaux;
 use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController {
@@ -18,9 +20,55 @@ class HomeController extends AbstractController {
    * @Route("/", name="home")
   */
   function home() {
-    return $this->render('home.html.twig', [
-        'message' => 'Bienvenue à la SPA'
-    ]);
+    try
+    {
+      $resultatArticles = $this->mysql
+      ->getRepository(Articles::class)
+      ->findAll();
+    }
+    catch ( Doctrine_Connection_Exception $e )
+    {
+      return $resultat = false;
+    }
+
+    try
+    {
+      $resultatAnimaux = $this->mysql
+      ->getRepository(Animaux::class)
+      ->findAll();
+    }
+    catch ( Doctrine_Connection_Exception $e )
+    {
+      return $resultat = false;
+    }
+
+    
+
+    if ($resultatAnimaux != false && $resultatArticles != false) {
+      for($counter = 0; $counter <= 5; $counter++) {
+        $returnArticles[$counter] = array(
+          "id" => $resultatArticles[$counter]->getId(),
+          "nom" => $resultatArticles[$counter]->getNom()
+        );
+      }
+
+      for($counter = 0; $counter <= 5; $counter++) {
+        $returnAnimaux[$counter] = array(
+          "id" => $resultatAnimaux[$counter]->getId(),
+          "nom" => $resultatAnimaux[$counter]->getNom(),
+          "age" => $resultatAnimaux[$counter]->getAge()
+        );
+      }
+
+      return $this->render('home.html.twig', [
+        'articles' => $returnArticles,
+        'animaux' => $returnAnimaux
+      ]);
+    }
+    else
+    {
+      return $this->redirectToRoute("home");
+    }
   }
 
 }
