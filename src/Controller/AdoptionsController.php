f1903161b91b8917cb35a172585fe5bd3329a7a1
Jean TALGORN--THOMAS <51022157+JeanTalgornThomas@users.noreply.github.com>
2021-06-25 10:28:29 +0200

    Système d'adoption

--- src/Controller/AdoptionsController.php
+++ src/Controller/AdoptionsController.php
@@ -6,6 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+
+use App\Entity\User;
+use App\Entity\Animaux;
 use App\Entity\Adoptions;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -14,6 +19,56 @@ class AdoptionsController extends AbstractController {
   public function __construct(EntityManagerInterface $mysql)
   {
     $this->mysql = $mysql;
+  }
+
+  /**
+   * @Route("/adopter/{id}", name="adopter")
+  */
+  function adopter($id, MailerInterface $mailer) {
+    $dons = new Adoptions();
+    $dons->setIdAnimaux($id);
+    $dons->setDate(new \DateTime());
+    $dons->setIdUtilisateurs($this->getUser()->getId());
+    $this->mysql->persist($dons);
+    $this->mysql->flush();
+
+    try
+    {
+      $resultatUtilisateur = $this->mysql
+      ->getRepository(User::class)
+      ->findOneBy(['id' => $this->getUser()->getId()]);
+    }
+    catch ( Doctrine_Connection_Exception $e )
+    {
+      return $resultatUtilisateur = false;
+    }
+
+    try
+    {
+      $resultatAnimal = $this->mysql
+      ->getRepository(Animaux::class)
+      ->findOneBy(['id' => $id]);
+    }
+    catch ( Doctrine_Connection_Exception $e )
+    {
+      return $resultatAnimal = false;
+    }
+
+    if ($resultatUtilisateur != false && $resultatAnimal != false)
+    {
+      $message = (new TemplatedEmail())
+      ->from('intranet-noreply@nodarius.fr')
+      ->to($resultatUtilisateur->getEmail())
+      ->subject('La SPA - Adoption d\'un animal')
+      ->htmlTemplate('emails/adoptions.html.twig')
+      ->context([
+          'nom_animal' => $resultatAnimal->getNom()
+      ]);
+    
+      $mailer->send($message);
+    }
+
+    return $this->render('adoptions.html.twig');
   }
 
 }
--- src/Controller/DonsController.php
+++ src/Controller/DonsController.php
@@ -6,7 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
 
+use App\Entity\User;
 use App\Form\DonsType;
 use App\Entity\Dons;
 use Doctrine\ORM\EntityManagerInterface;
@@ -21,7 +24,7 @@ class DonsController extends AbstractController {
   /**
    * @Route("/addDons", name="addDons")
   */
-  function addDons(Request $request): Response {
+  function addDons(Request $request, MailerInterface $mailer): Response {
     date_default_timezone_set('Europe/Paris');
     
     $dons = new Dons();
@@ -34,10 +37,31 @@ class DonsController extends AbstractController {
         $dons = $form->getData();
         $dons->setMontants($dons->getMontants());
         $dons->setDate(new \DateTime());
-        $dons->setIdUtilisateurs(1);
-            
+        $dons->setIdUtilisateurs($this->getUser()->getId());
         $this->mysql->persist($dons);
         $this->mysql->flush();
+
+        try
+        {
+          $resultatUtilisateur = $this->mysql
+          ->getRepository(User::class)
+          ->findOneBy(['id' => $this->getUser()->getId()]);
+        }
+        catch ( Doctrine_Connection_Exception $e )
+        {
+          return $resultatUtilisateur = false;
+        }
+
+        $message = (new TemplatedEmail())
+        ->from('intranet-noreply@nodarius.fr')
+        ->to($resultatUtilisateur->getEmail())
+        ->subject('La SPA - Confirmation de dons')
+        ->htmlTemplate('emails/dons.html.twig')
+        ->context([
+            'dons' => $dons->getMontants()
+        ]);
+      
+        $mailer->send($message);
 
         return $this->redirectToRoute('home');
     }
